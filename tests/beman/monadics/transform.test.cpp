d45f99dae766702c0b24226b0ccdc5a71f8f97d6
Mikhail Svetkin <trilla@Mac.lan>
2025-08-11 22:22:36 +0200

    u(repo): transform

--- tests/beman/monadics/transform.test.cpp
+++ tests/beman/monadics/transform.test.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "beman/monadics/transform.hpp"
+#include "beman/monadics/concepts/instance_of.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -18,6 +19,52 @@ TEST_CASE("opt-value") {
     STATIC_REQUIRE(traits::has_value(box2));
     STATIC_REQUIRE(traits::value(box2) == 30);
     STATIC_REQUIRE(traits::support_error == false);
+}
+
+} // namespace beman::monadics::tests
+
+template <typename T, typename E>
+struct Result {
+    using Data = std::conditional_t<std::is_void_v<T>, std::variant<std::monostate, E>, std::variant<T, E> >;
+
+    Data data;
+};
+
+// template<typename T, typename E>
+// struct beman::monadics::box_traits<Result<T, E>> {
+template <beman::monadics::instance_of<Result> T>
+struct beman::monadics::box_traits<T> {
+    inline static constexpr bool has_value(const auto& box) noexcept { return box.data.index() == 0; }
+
+    inline static constexpr decltype(auto) value(auto&& box) noexcept {
+        using U = decltype(std::get<0>(std::forward<decltype(box)>(box).data));
+
+        if constexpr (!std::same_as<std::remove_cvref_t<U>, std::monostate>) {
+            return std::get<0>(std::forward<decltype(box)>(box).data);
+        }
+    }
+
+    inline static constexpr decltype(auto) error(auto&& box) noexcept {
+        return std::get<1>(std::forward<decltype(box)>(box).data);
+    }
+};
+
+namespace beman::monadics::tests {
+
+TEST_CASE("opt-result") {
+    using T      = Result<void, int>;
+    using traits = box_traits_for<T>;
+
+    STATIC_REQUIRE(std::same_as<traits::value_type, void>);
+    STATIC_REQUIRE(std::same_as<traits::error_type, int>);
+
+    constexpr T box{};
+
+    constexpr auto box2 = box | transform([]() { return std::string_view{"test"}; });
+
+    STATIC_REQUIRE(traits::has_value(box2));
+    STATIC_REQUIRE(traits::value(box2) == "test");
+    STATIC_REQUIRE(traits::support_error == true);
 }
 
 } // namespace beman::monadics::tests
