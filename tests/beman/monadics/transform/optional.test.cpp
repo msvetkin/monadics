a1ff6815ee20d63f728c37dbddc1c580b2fd9f4b
Mikhail Svetkin <trilla@apollo.local>
2025-10-03 13:18:21 +0200

    re-org transform

---
+++ tests/beman/monadics/transform/optional.test.cpp
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "beman/monadics/detail/transform.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "helpers/optional-full.hpp"
+
+namespace beman::monadics::tests {
+
+TEST_CASE("has-box-traits") {
+    STATIC_REQUIRE(has_box_traits<std::optional<int>>);
+}
+
+TEST_CASE("with-value") {
+  constexpr auto result = std::optional{10}
+      | transform([] (auto &&value) {
+          return value * 2.0;
+      });
+  STATIC_REQUIRE(std::same_as<decltype(result), const std::optional<double>>);
+  STATIC_REQUIRE(result.has_value());
+  STATIC_REQUIRE(result.value() == 20.0);
+}
+
+TEST_CASE("with-nullopt") {
+  constexpr auto result = std::optional<int>{}
+      | transform([] (auto &&value) {
+          return (value - 1) * 0.5;
+      });
+  STATIC_REQUIRE(std::same_as<decltype(result), const std::optional<double>>);
+  STATIC_REQUIRE(result.has_value() == false);
+}
+
+} // namespace beman::monadics::tests
