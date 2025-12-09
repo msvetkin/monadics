1dcd86f065a1145555007d82119426262241bd6e
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2025-12-09 22:22:19 +0100

    u(examples): update simple example

--- examples/monadics_std_usage.cpp
+++ examples/monadics_std_usage.cpp
@@ -2,4 +2,21 @@
 
 #include <beman/monadics/monadics.hpp>
 
-int main() { return 0; }
+#include <cstdlib>
+#include <optional>
+
+template <typename T>
+struct beman::monadics::box_traits<std::optional<T>> {
+    [[nodiscard]] inline static constexpr auto error() noexcept { return std::nullopt; }
+};
+
+int main() {
+    namespace bms = beman::monadics;
+
+    const auto result = std::optional{10}
+                      | bms::and_then([](auto&& v) { return std::optional{std::forward<decltype(v)>(v) * 2.0}; })
+                      | bms::transform([](auto&& v) { return static_cast<int>(v); })
+                      | bms::and_then([](auto) { return std::optional<char>{}; })
+                      | bms::or_else([]() { return std::optional{EXIT_SUCCESS}; });
+    return result.value_or(EXIT_FAILURE);
+}
